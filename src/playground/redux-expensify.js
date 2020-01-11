9ebbd362e4bfc680eb477209b898b409f65ddb18
Seth Poulin <seth.poulin@gmail.com>
2020-01-11 16:49:24 -0500

    completed redux store.  Not yet refactored.

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -157,6 +157,14 @@ const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
 
     
     return startDateMatch && endDateMatch && textMatch;
+  }).sort((a, b) => {
+    if (sortBy === 'date') {
+      return a.createdAt < b.createdAt ? 1 : -1
+    };
+    if (sortBy === 'amount') {
+      return a.amount > b.amount ? -1 : 1
+    };
+
   });
 };
 
@@ -175,22 +183,22 @@ store.subscribe(() => {
   console.log(visibleExpenses);
 });
 
-const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100, createdAt: 1000 }));
-const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 300, createdAt: 1001 }));
+const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100, createdAt: -1000 }));
+const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 300, createdAt: -1500 }));
 
 // store.dispatch(removeExpense({ id: expenseOne.expense.id }));
 
 // store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
 
-store.dispatch(setTextFilter('rent'));
+// store.dispatch(setTextFilter('rent'));
 // store.dispatch(setTextFilter(''));
 
-// store.dispatch(sortByAmount());
+store.dispatch(sortByAmount());
 // store.dispatch(sortByDate());
 
-// store.dispatch(setStartDate(0));
+// store.dispatch(setStartDate(-1000));
 // store.dispatch(setStartDate());
-// store.dispatch(setEndDate(1250));
+// store.dispatch(setEndDate(-5000));
 
 const demoState = {
   expenses: [{
