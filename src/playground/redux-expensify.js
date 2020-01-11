7fffec9dd9dbb231810db1b37cbec6a6222e9bf6
Seth Poulin <seth.poulin@gmail.com>
2020-01-11 14:26:27 -0500

    continue building out reducers and functions.

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,17 +1,54 @@
 import { createStore, combineReducers } from 'redux';
+import uuid from 'uuid';
 
 //ADD_EXPENSE
-const addExpense = () => ({
+const addExpense = (
+  { 
+    description = '', 
+    notes = '', 
+    amount = 0, 
+    createdAt = 0 
+  } = {}
+) => ({
   type: 'ADD_EXPENSE',
   expense: {
-    
+    id: uuid(),
+    description,
+    //same as description: description
+    notes,
+    amount,
+    createdAt
   }
 });
 
 
+
+
 //REMOVE_EXPENSE
+
+const removeExpense = (
+  {
+    id: id
+  }  = {}
+) => ({
+  type: 'REMOVE_EXPENSE',
+  id: id
+});
 //EDIT_EXPENSE
+
+const editExpense = (id, updates) => ({
+  type: 'EDIT_EXPENSE',
+  id: id,
+  updates: updates
+});
+
 //SET_TEXT_FILTER
+
+const setTextFilter = (text) => ({
+  type: 'SET_TEXT_FILTER',
+  text
+});
+
 //SORT_BY_DATE
 //SORT_BY_AMOUNT
 //SET_START_DATE
@@ -23,6 +60,29 @@ const expensesReducerDefaultState = [];
 
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
   switch(action.type){
+    case 'ADD_EXPENSE':
+      return [
+        ...state,
+        action.expense
+      ];
+    case 'REMOVE_EXPENSE':
+      // console.log(action.id);
+      return state.filter((expense) => {
+        return action.id !== expense.id 
+      });
+    case 'EDIT_EXPENSE':
+      return state.map((expense) => {
+        if(expense.id === action.id){
+          return {
+            ...expense,
+            //this takes everything from the original expense object
+            ...action.updates
+            //and this adds on properties which then override the original ones
+          }
+        } else {
+          return expense
+        }
+      })
     default:
       return state;
   }
@@ -39,6 +99,11 @@ const filtersReducerDefaultState = {
 
 const filtersReducer = (state = filtersReducerDefaultState, action) => {
   switch(action.type){
+    case 'SET_TEXT_FILTER':
+      return {
+        ...state,
+        text: action.text
+      };
     default:
       return state;
   }
@@ -54,7 +119,22 @@ const store = createStore(
   })
 );
 
-console.log(store.getState());
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100 }));
+const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 300 }));
+
+store.dispatch(removeExpense({ id: expenseOne.expense.id }));
+
+store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
+
+store.dispatch(setTextFilter('rent'));
+store.dispatch(setTextFilter(''));
+
+store.dispatch(sortByAmount());
+store.dispatch(sortByDate());
 
 const demoState = {
   expenses: [{
@@ -71,3 +151,14 @@ const demoState = {
     endDate: undefined
   }
 };
+
+const person = {
+  name: 'Jen',
+  age: 24
+};
+
+console.log ({
+  ...person,
+  location: 'Canada',
+  age: 27
+});
